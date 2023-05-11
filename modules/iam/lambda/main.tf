d406034d38230d03fca3d44ca23a51cc018818b8
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2023-05-11 21:14:11 +0900

    ポリシー追加 (#6)
    * iamポリシー追加
    
    * Update main.tf
    
    * Update tfsec_config.yml

--- modules/iam/lambda/main.tf
+++ modules/iam/lambda/main.tf
@@ -11,6 +11,48 @@ resource "aws_iam_role" "template" {
   }
 }
 
+resource "aws_iam_role_policy" "template" {
+  name = "minutes-serverless-prod-lambda"
+  role = var.iam_role_name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:TagResource"
+        ]
+        Resource : [
+          "arn:aws:logs:ap-northeast-1:345185900516:log-group:/aws/lambda/minutes-serverless-prod*:*"
+        ],
+        Effect : "Allow"
+      },
+      {
+        Action : [
+          "logs:PutLogEvents"
+        ],
+        Resource : [
+          "arn:aws:logs:ap-northeast-1:345185900516:log-group:/aws/lambda/minutes-serverless-prod*:*:*"
+        ],
+        Effect : "Allow"
+      },
+      {
+        Sid : "VisualEditor0",
+        Effect : "Allow",
+        Action : [
+          "kms:Decrypt",
+          "ssm:GetParameter"
+        ],
+        Resource : [
+          "arn:aws:kms:ap-northeast-1:345185900516:key/cec7b1ba-32d0-48ec-a014-51694fd24e33",
+          "arn:aws:ssm:ap-northeast-1:345185900516:parameter/*"
+        ]
+      }
+    ]
+  })
+}
+
 data "aws_iam_policy_document" "lambda_assume_policy" {
   statement {
     effect = "Allow"
